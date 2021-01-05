68c90cfcc45e7afa05e71770b5fcfd7ad34d4bc9
nvstco <ivanisaiahserato@gmail.com>
2021-01-05 18:01:51 +0800

    Updated talk.js
    Changed embed keyword.

--- commands/talk.js
+++ commands/talk.js
@@ -5,7 +5,7 @@
         if (message.author.id == "170501385954131968")  {
         if (message.deletable) message.delete()
             var msg_ = message.content.slice(5)
-            if (args[1] == "mb") {
+            if (args[1] == "/m") {
                 const sayEmbed = new Discord.MessageEmbed()
                     .setTitle(msg_.slice(args[1].length+1))
                     .setColor("#fafafa")
