4fd2a9c41cb392c2b29a7ba948f88c475bb410a6
nvstco <ivanisaiahserato@gmail.com>
2021-01-05 17:33:32 +0800

    Fixed Command Handler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 const token = 'NjkzMTAzMjU1MzQ5MjMxNjc3.Xn8e_A.TAWu2_-KzJoI5HInqZKOfdQ_H50';
 
 console.log('');
@@ -7,11 +8,8 @@ const prefix = "=";
 
 client.on('ready', ()=>{
     console.log('  ❱❱ Online.');
-    client.user.setActivity(`with the clouds`)
+    client.user.setActivity(`with the clouds.`)
 })
-
-
-    //#region commandhandler
 
     const fs = require('fs');
     client.commands = new Discord.Collection();
@@ -26,92 +24,20 @@ client.on('ready', ()=>{
             client.commands.set(command.help.name, command);
         }
     } catch(err) {
-        console.log(err.message)
+        console.log(err)
     }
-    //#endregion
-    
 
-    client.on('message', async message=>{
+client.on('message', async message=>{
 
     const args = message.content.substring(prefix.length).split(" ")
+
     if (message.author.bot) return;
     if (!message.guild) return;
     if (!message.content.startsWith(prefix)) return;
     
-    let cmd = client.commands.get(args[0]); //slices the PREFIX
+    let cmd = client.commands.get(args[0]);
     if(cmd) cmd.run(client, message, args.slice(1));
-
-
-    /*
-    switch(args[0]){
-        
-        case "talk":
-            if (message.deletable) message.delete()
-            var msg_ = message.content.slice(args[0].length+1)
-            if (args[1] == "mb") {
-                const sayEmbed = new Discord.MessageEmbed()
-                    .setTitle(msg_.slice(args[1].length+1))
-                    .setColor("#fafafa")
-                message.channel.send(sayEmbed)
-            }
-            else
-                message.channel.send(msg_)
-            break;
-
-        case "help":
-            var data = require('fs').readFileSync(`${path}/archives/help.txt`).toString()
-            message.channel.send(data)
-            break;
-
-        case "format":
-            if (message.author.id == "170501385954131968")  {
-                if (message.deletable) message.delete()
-                try {
-                    var file_ = `${path}/CC001/CH/${message.content.slice(args[0].length+2)}.txt`
-                    var data = require('fs').readFileSync(file_).toString().split("\n");
-                    for (i = 0; i<data.length; i++) data[i] = data[i].substring(data[i].indexOf(":")+1)
-
-                    try {
-                        var code = `${path}/CC001/CODE/${message.content.slice(args[0].length+2)} A.txt`
-                        var code_ = require('fs').readFileSync(code).toString()
-                    } catch {
-                        console.log("  ❱❱ Format file does not exist.") }
-
-                    const ccEmbed = new Discord.MessageEmbed()
-                        .setTitle(data[0]).setDescription(data[1])
-                        .addFields(
-                            { name: '**Constraints:**',     value: `\n ${data[3]}` },
-                            { name: '**Example:**',   value: `\n ${data[4]}` },
-                            { name: '**Format:**', value: `\n ${code_}` },
-                            { name: '**Deadline:**',        value: `\n ${data[5]}` }
-                        )
-                        .setColor(data[2]).setFooter(data[6])
-                        .setThumbnail("https://i.imgur.com/zUlXRHd.png")
-                    message.channel.send(ccEmbed)
-                } catch { 
-                    message.channel.send("File does not exist.")
-                    console.log("  ❱❱ File does not exist.") }
-                    
-            }
-            break;
-        
-        case "ts":
-            var txt = message.content.slice(args[0].length+1).split("|");
-            const ts = new Discord.MessageEmbed()
-                .setTitle(txt[0])
-                .setDescription(txt[1])
-                //.setColor()
-                //.setFooter()
-                //.setImage()
-                //.setThumbnail()
-                //.setTimestamp()
-            message.channel.send(ts);
-                
-            break;
-        }
-        */
     
 })
 
 client.login(token);
-//client.commands.get(args[0]);
