068fc45bfd332d188a5f81b9e1893717a3ae0ca1
nvstco <ivanisaiahserato@gmail.com>
2021-09-17 19:22:56 +0800

    Streamline help.js description handler
    Modified to read the description of a command instead of a .json file.

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,25 +1,41 @@
 
     const Discord = require('discord.js');
-    module.exports.run = async (client, message, args) => {
-        
-        // extracts text from help.json
-        const { avatar, formatter, help, invite, math, ping, rformat, talk, tone } = require('../../files/help.json')
-        const user = message.mentions.users.first() || message.author;
+    const { prefix } = require('../../preferences.json');
 
+    module.exports.run = async (client, message, args) => {
+
+        const user = message.mentions.users.first() || message.author;
         const hpEmbed = new Discord. MessageEmbed ()  
             .setAuthor("Arkus Commands", "https://cdn.discordapp.com/attachments/693499542850764870/885917793869004800/954b5761a1795b57fc05c2e262ae2fd2.gif")
             .setDescription(`The prefix for this server is \`=\`. Note that all commands are experimental and only few work as of the moment.`)
-            .addField("Basic Commands", `${avatar}\n${formatter}\n${help}\n${invite}\n${math}\n${ping}\n${rformat}\n${talk}\n${tone}`)
 
             .setColor(message.guild.me.displayHexColor)
             .setFooter(`Requested by ${message.author.username}.`, user.avatarURL())
             .setThumbnail(message.guild.iconURL())
+
+        const fs = require('fs');
+        const c_master = fs.readdirSync('./commands/');
+        for(let c_folder of c_master) {
+
+            const c_files = fs.readdirSync(`./commands/${c_folder}`)
+                .filter(file => file.endsWith('.js'));
+
+            for (const file of c_files){
+                const command = require(`../../commands/${c_folder}/${file}`);
+
+                var alias_arr = [];
+                command.help.aliases.forEach(alias => {
+                    alias_arr += `, \`${prefix + alias}\` `
+                });
+                hpEmbed.addField(`\`${prefix + command.help.name}\`${alias_arr}`,`>     ✦ ${command.help.description}`);
+            }
+        }
 
         message.channel.send(hpEmbed);
     }
 
     module.exports.help = {
         name: "help",
-        aliases: ['h'],
-        description: "Opens the commands catalog of the bot."
+        aliases: ['h', 'commands', 'cmd'],
+        description: "Displays all the commands supported by the bot."
     }
