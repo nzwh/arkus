3f560770269ed69b8e04683e5adc325b9f5723b3
nvstco <ivanisaiahserato@gmail.com>
2021-09-19 06:53:24 +0800

    Split math.js commands
    Moved math.js commands to different files. Added a functions.js file.

---
+++ commands/math/prime.js
@@ -0,0 +1,23 @@
+    
+    const { primeGen } = require('../../files/functions.js')
+    module.exports.run = async (client, message, args) => {
+        
+        if (args.some(isNaN)) 
+            message.channel.send("> **Invalid input.** Must be a numerical value.");
+
+        else if (args.length > 1) 
+            message.channel.send("> Input only one numerical value.");
+
+        else {
+            if (primeGen(args[0])) 
+                message.channel.send(`\`\`\`py\n✦ The number ${args[0]} is a prime.\`\`\``);
+            else 
+                message.channel.send(`\`\`\`py\n✦ The number ${args[0]} is not a prime.\`\`\``);
+        }
+    }
+
+    module.exports.help = {
+        name: "prime",
+        aliases: ['pr'],
+        description: "Returns if a given number is prime or not."
+    }
---
+++ commands/math/prime_factors.js
@@ -0,0 +1,29 @@
+    
+    const { factorGen, primeGen } = require('../../files/functions.js')
+    module.exports.run = async (client, message, args) => {
+        
+        if (args.some(isNaN)) 
+            message.channel.send("> **Invalid input.** Must be a numerical value.");
+
+        else if (args.length > 1) 
+            message.channel.send("> **Invalid input.** Input only one value.");
+
+        else if (args[0] > 1000 && args[0] <= 3) 
+            message.channel.send("> **Invalid input.** Must be a number between 3 and 1000.");
+
+        else {
+            var pfactor_arr = factorGen(args[0]), output = "";
+            pfactor_arr.forEach(p_factor => {
+                if(primeGen(p_factor) && p_factor >= 2) output += `${p_factor}, `;
+            });
+
+            message.channel.send(`\`\`\`q\n✦ Prime Factors of ${args[0]}: ${output.slice(0, -2)}\`\`\``);
+        }
+
+    }
+
+    module.exports.help = {
+        name: "pfactor",
+        aliases: ['pf', 'primefactors'],
+        description: "Returns a list of prime factors of a given input."
+    }
